f42820638099a7f85f5647ba081917d0e0080d80
Chet Farley <cfarley@aislerocket.com>
2022-05-29 01:29:53 -0500

    [in progress] pulling in multiple fields from airtable API

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ console.log('hello world poop hi hi ')
 // airtable configs
 import Airtable from 'airtable';
 var base = new Airtable({apiKey: 'keymozIRq1SpsUPa6'}).base('appIn0GQksTbRvLRb');
-
+/* everything workeing perfectly
 // List Dice Steps Records
 const table = base('Dice Steps');
 
@@ -26,3 +26,28 @@ const getRecords = async () => {
 }
 
 getRecords();
+*/
+
+
+const getRecords = async (table, cell) => {
+    const records = await base(table).select({
+        // Only data for fields whose names are in this list will be included in the result.
+        fields: [cell]
+    }).eachPage(function page(records, fetchNextPage) {
+        records.forEach(function(record) {
+            console.log('retrieved', record.get(cell))
+            
+            // Print Records into a list
+            let item = record.get(cell);
+            const ul = document.getElementById(column + '-list');
+            var li = document.createElement('li');
+            li.appendChild(document.createTextNode(item));
+            ul.appendChild(li);
+        })
+    });
+}
+
+// List Dice Steps Records
+const diceTable = 'Dice Steps';
+let diceCell = 'Dice';
+getRecords(diceTable, diceCell);
